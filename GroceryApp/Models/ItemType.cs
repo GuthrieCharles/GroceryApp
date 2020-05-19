aa794c76c453eb4440bef2f40c242869919f1a61
Charles Guthrie <GuthrieC@sbcglobal.net>
2020-05-19 15:35:41 -0500

    Add Classes to Models

---
+++ GroceryApp/Models/ItemType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryApp.Models
+{
+    public enum ItemType
+    {
+        Each,
+        Pounds,
+        Cans,
+        Bottle,
+        Bunch,
+        Pack
+    }
+}
---
+++ GroceryApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public ItemType? NumDescription { get; set; }
+        public bool? OnSale { get; set; }
+        public int Quanity { get; set; }
+
+
+    }
+}
